1195eb03ed5aac5576d2514ae16e3ccf57bff336
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2018-11-20 12:46:42 +0000

    give somw shadow to logo and header

--- js/fruits_n_flowers.js
+++ js/fruits_n_flowers.js
@@ -7,8 +7,34 @@ function start() {
     $(".logo").hide();
     $("header").show();
 
-    console.log(levels.length);
+    createLevelTable();
+
 } // end of start
+
+function createLevelTable(selector, rowNum, cellNum) {
+    tbody = document.createElement("tbody");
+
+    // create rows
+    for (r = 0; r < 5; r++) {
+        const row = document.createElement("tr");
+
+        // create cells
+        for (c = 0; c < 5; c++) {
+            const cell = document.createElement("td");
+
+            cell.id = `level-btn-${r}-${c}`;
+            cell.innerHTML = `${r * 5 + c}`;
+
+            row.append(cell);
+        } // end of for cell
+
+        tbody.append(row);
+    } // end of for row
+
+    $(".level-table").append(tbody);
+
+    console.log($(".level-table").get());
+} // end of createLevelTable
 
 
 /*
@@ -21,5 +47,5 @@ function start() {
 var levels = [
     {}, {}, {}, {}, {}, {}, {}, {}, {}, {},
     {}, {}, {}, {}, {}, {}, {}, {}, {}, {},
-    {}, {}, {}, {}, {}, {}
+    {}, {}, {}, {}
 ]
