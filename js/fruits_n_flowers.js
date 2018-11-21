f0e5d57f6c6351024554044aa29a0a07350dbec5
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2018-11-21 05:19:31 +0000

    level panel is compete

--- js/fruits_n_flowers.js
+++ js/fruits_n_flowers.js
@@ -9,6 +9,7 @@ function start() {
 
     createLevelTables();
     createLevelPageIndicator();
+    styleLevelMenuToCurrenPage();
     addLevelEvents();
 } // end of start
 
@@ -30,11 +31,11 @@ function createLevelPageIndicator() {
         const indicator = document.createElement("div"),
             light = document.createElement("div");
 
-        $(indicator).addClass("level-indicator");
-        $(light).addClass("level-light");
+        $(indicator).addClass(`level-indicator to-level-page-${i + 1}`);
+        $(light).addClass(`level-light to-level-page-${i + 1}`);
 
         if (i == 0) {
-            $(indicator).addClass("level-indicator--active");
+            $(light).addClass("level-light--active");
         }
 
         $(indicator).append(light);
@@ -107,6 +108,16 @@ function createLevelTables() {
 function addLevelEvents() {
     // add eventlistener to arrows, there are only two of them, no need to delegate
     $(".level-menu__arrow-left").on("click", () => { turnLevelPage("-") });
+    $(".level-menu__arrow-right").on("click", () => { turnLevelPage("+") });
+
+    $(".level-menu__header__level-indicator").on("click", function (event) {
+        // event is delegated by common class name to-level-page-n
+        if (/to-level-page-/g.test($(event.target).attr("class"))) {
+            const pageNum = $(event.target).attr("class").match(/to-level-page-\d+/g)[0]
+                .match(/\d+/)[0]; // extract page number from class name
+            turnLevelPage(pageNum);
+        } // end of if target is valid elem
+    }); // end of indicator click
 } // end of addLevelEvents
 
 function turnLevelPage(turnTo) {
@@ -118,19 +129,47 @@ function turnLevelPage(turnTo) {
         pageNum = activePage.id.match(/\d+/)[0];
 
     if (turnTo === "+") {
-        displayPage(Number(pageNum) + 1);
+        displayPage(Number(pageNum) + 1, activePage);
     } else if (turnTo === "-") {
-        displayPage(Number(pageNum) - 1);
+        displayPage(Number(pageNum) - 1, activePage);
     } else if (Number(turnTo)) {
-        displayPage(turnTo);
+        displayPage(turnTo, activePage);
     } else throw Error("turnLevelPage function got unvalid parameters : [" + turnTo + "]!");
 } // end of turnLevelPage
 
-function displayPage(pNum) {
-    console.log(pNum);
-}
 
 
+function displayPage(pNum, currentActive) {
+    const lastPage = Math.ceil(levels.length / 25);
+
+    if (pNum > 0 && pNum <= lastPage) {
+        $(currentActive).removeClass("active-page");
+        const newActive = $("#level-page-" + pNum).addClass("active-page")[0];
+        styleLevelMenuToCurrenPage();
+    } // end of if num in range
+} // end of displayPage
+
+
+
+function styleLevelMenuToCurrenPage() {
+    const activePageNum = $(".active-page")[0].id.match(/\d+/g)[0];
+
+    // hightlight the right page indicator
+    $(".level-light--active").removeClass("level-light--active");
+    $(".level-light.to-level-page-" + activePageNum).addClass("level-light--active");
+
+    // activate available arrows
+    activePageNum > 1
+        ? $(".level-arrow-left").addClass("level-arrow-left--active")
+        : $(".level-arrow-left").removeClass("level-arrow-left--active");
+    activePageNum < Math.ceil(levels.length / 25)
+        ? $(".level-arrow-right").addClass("level-arrow-right--active")
+        : $(".level-arrow-right").removeClass("level-arrow-right--active");
+
+    // change footer text accordingly
+    $(".level-menu__footer__text-container")
+        .html(`-&lt;[&nbsp;&nbsp;${activePageNum} / ${Math.ceil(levels.length / 25)}&nbsp;&nbsp;]&gt;-`);
+} // end of styleLevelMenu
 /*
 
         LEVELS
